44070b1ff7ac99215d2f5e062cf5819b73766d73
Las <bsantana.it@gmail.com>
2022-02-26 01:25:41 -0300

    Create lables.php

---
+++ resources/lang/pt-br/lables.php
@@ -0,0 +1,319 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+
+    'general-backtotop' => 'Back To Top',
+
+
+
+    'header-top-offer' => 'Ganhe um<strong> DESCONTO DE 10% </strong>Na sua primeira compra!
+    <div class="pro-link-dropdown js-toppanel-link-dropdown">
+      <a href="/shop" class="pro-dropdown-toggle" style="cursor:pointer">
+        Clique Aqui!
+      </a>
+    </div>',
+    'header-welcome-text' => 'Welcome',
+    'header-welcome-user' => 'Seja Bem Vindo!',
+    'header-login-register' => 'Login/Cadastrar',
+    'header-login' => 'Login',
+    'header-register' => 'Register',
+    'header-my-account' => 'My Account',
+    'header-profile' => 'Profile',
+    'header-wishlist' => 'Wishlist',
+    'header-compare' => 'Compare',
+    'header-order' => 'Orders',
+    'header-shipping-address' => 'Shipping Address',
+    'header-change-password' => 'Change Password',
+    'header-logout' => 'Logout',
+    'header-all-categories' => 'CATEGORIAS',
+    'header-cart' => 'Sacola',
+    'header-emptycart' => 'You have no items in your shopping cart.',
+    'header-item' => 'Itens',
+    'header-total' => 'Total',
+    'header-checkout' => 'Checkout',
+    'header-view-cart' => 'View Cart',
+    'header-search-products' => 'Buscar',
+    'header-call-us-now' => 'Call Us Now',
+    'header-free-shipping' => 'Free Shipping',
+    'header-over-order' => 'On order over $99',
+    'header-header2-phone' => 'Contato Direto',
+
+    'home-slider-button-title'=>"Botão de Comprar",
+
+    'home-product-categories-title'=>"PRODUCT CATEGORIES",
+    'home-product-categories-description'=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis felis tempus feugiat maximus.",
+    'home-banner-title'=>"SNEAKER",
+    'home-banner-sub-title'=>"Couples Footwear",
+    'home-button-text'=>"View All Range",
+
+    'home-new-arrival-title'=>"NEW ARRIVAL",
+    'home-new-arrival-description'=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis felis tempus feugiat maximus.",
+
+    'home-news-title'=>"NEWS & EVENTS",
+    'home-news-description'=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Morbi venenatis felis tempus feugiat maximus.",
+
+    'home-tab-title'=>"PERFEITOS PARA PRESENTEAR",
+    'home-tab-description'=>"Aproveite as promoções da Andressa Cosméticos. Presenteie quem você ama com os melhores produtos.",
+    'home-tab-topsales'=>"Top Sales",
+    'home-tab-special'=>"Special",
+    'home-tab-most-liked' => 'Most Liked',
+
+    'home-tab-top-title'=>"CATEGORIA DE PRODUTOS ",
+    'home-tab-top-description'=>"A mais completa linha de produtos para você que é exigente.",
+
+    'home-servics-free-shipping'=>"Frete Grátis",
+    'home-servics-on-ordr'=>"Acima de R$100,00",
+    'home-servics-money-return'=>"Compra Garantida",
+    'home-servics-return-days'=>"Compre com segurança",
+    'home-servics-support'=>"Suporte Personalizado",
+    'home-servics-hotline'=>"Ligue : (62) 9 9932-3372",
+    'home-servics-safe-payment'=>"Pagamento Fácil",
+    'home-servics-protect-payment'=>"Cartão, Pix e outros",
+
+
+
+    'home-weekly-sale-heading'=>"TOP SELLING OF THE WEEK",
+    'home-weekly-sale-description'=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis felis tempus feugiat maximus.",
+
+
+
+    'footer-quick-links'=>'Quick Links',
+    'footer-home'=>'Home',
+    'footer-shop'=>'Produtos',
+    'footer-orders'=>'Orders',
+    'footer-shopping-cart'=>'Shopping Cart',
+    'footer-wishlist'=>'Wishlist',
+    'footer-personalization' => 'Personalization',
+    'footer-instagram-feed' => 'Instagram Feed',
+    'footer-company' => 'Website 2021, All Rights Reserved.',
+    'footer-privacy' => 'Privacy',
+    'footer-term' => 'Terms',
+    'footer-about-store' => 'About Store',
+    'footer-our-services' => 'Our Services',
+    'footer-information' => 'Information',
+    'footer-newsletter' => 'Newsletter',
+    'footer-follow-us' => 'Follow Us',
+    'footer-download-our-app' => 'DOWNLOAD OUR APP',
+    'footer-category' => 'categories',
+    'footer-payments'=>'We Using safe payments',
+    'footer-contact-us' => 'Contato',
+    'footer-latest-subscribe'=>'SUBSCRIBE FOR LATEST UPDATES',
+    'footer-subscribe' => 'Subscribe',
+    'footer-safe-payment' => 'We Using safe payments',
+    'footer-facebook' => 'Facebook',
+    'footer-twitter' => 'Twitter',
+    'footer-google' => 'Google',
+    'footer-linkedin' => 'LinkedIn',
+    'footer-instagram' => 'Instagram',
+
+
+
+
+
+
+
+
+
+
+
+
+
+    'shop-shop'=>'Shop',
+    'shop-display' => 'Display',
+    'shop-category' => 'Category',
+    'shop-price' =>'Price',
+    'shop-sort-by' => 'Sort by',
+    'shop-brands' => "Brand",
+    'shop-apply'=>'Apply',
+    'shop-reset'=>'reset',
+
+
+    'cart-page-shopping-cart'=>'Shopping Cart',
+    'cart-page-apply'=>'Apply',
+    'cart-page-continue-shopping'=>'CONTINUE SHOPPING',
+    'cart-page-update-cart'=>'UPDATE CART',
+    'cart-page-proceed-to-checkout'=>'PROCEED TO CHECKOUT',
+    'cart-page-order-summary'=>'Order Summary',
+    'cart-page-subtotal'=>'Subtotal',
+    'cart-page-discount'=>'Discount(Coupon)',
+    'cart-page-total'=>'Total',
+
+
+    'product-detail-product'=>"Product",
+    'product-detail-related-product-title'=>'Related Products',
+    'product-detail-related-product-description'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Morbi venenatis felis tempus feugiat maximus.',
+
+
+
+
+    'checkout-checkout'=>'CHECKOUT',
+    'checkout-shipping-address'=>'Shipping Address',
+    'checkout-billing-address'=>'Billing Address',
+    'checkout-shipping-method'=>'Shipping Methods',
+    'checkout-order-detail'=>'Order Details',
+
+
+    'checkout-first-name'=>'First Name',
+    'checkout-last-name'=>'Last Name',
+    'checkout-address'=>'Address',
+    'checkout-country-name'=>'Country Name',
+    'checkout-state-name'=>'State Name',
+    'checkout-city-name'=>'City Name',
+    'checkout-postal-code'=>'Postal Code',
+    'checkout-phone'=>'Phone',
+
+
+    'checkout-billing-first-name'=>'First Name',
+    'checkout-billing-last-name'=>'Last Name',
+    'checkout-billing-country-name'=>'Country Name',
+    'checkout-billing-state-name'=>'State Name',
+    'checkout-billing-city-name'=>'City Name',
+    'checkout-billing-postal-code'=>'Postal Code',
+    'checkout-billing-phone'=>'Phone',
+    'shipping-address-latlong'=>'Lat/Lng',
+    'checkout-continue'=>'Continue',
+    'checkout-back'=>'Back',
+
+
+    'checkout-order-notes-title'=>'Order Notes & Summary',
+    'checkout-order-notes-description'=>'Please write notes of your
+    order',
+    'checkout-payment-method-title'=>'Payment Methods',
+    'checkout-payment-method-description'=>'Shipping Methods',
+
+
+    'checkout-order-summary'=>'Order Summary',
+    'checkout-subtotal'=>'Subtotal',
+    'checkout-discount'=>'Discount(Coupon)',
+    'checkout-total'=>'Total',
+    'checkout-shipping'=>'Shipping',
+    'checkout-tax'=>"Tax",
+    'checkout-same-ship-bill-address-text'=>'Same shipping and billing address.',
+    'checkout-location' => 'Location',
+    'checkout-location-placeholder'=>'Please enter your location or click here to open map',
+
+    'login-sign-up' => 'Sign Up',
+    'login-login' => 'Login',
+    'login-email' =>'Email Address',
+    'login-password' => 'Password',
+    'login-new-customer' => 'New Customer',
+    'login-first-name' => 'First Name',
+    'login-last-name' => 'Last Name',
+    'login-confirm-password' => 'Confirm Password',
+    'login-create-account' => 'Create An Account',
+    'login-access-account' => 'Access Your Account Through Your Social Networks',
+    'login-forget-password' =>'Forgot Password',
+
+    'profile-side-menue-profile' =>'Profile',
+    'profile-side-menue-wishlist' =>'Wishlist',
+    'profile-side-menue-compare' =>'Compare',
+    'profile-side-menue-orders' =>'Orders',
+    'profile-side-menue-shipping-address' =>'Shipping Address',
+    'profile-side-menue-change-password' =>'Change Password',
+    'profile-side-menue-logout' =>'Logout',
+
+
+    'profile-email'=> "Email",
+    'profile-my-account'=> "My Account",
+    'profile-prsonal-info'=> "Personal Information",
+    'profile-first-name' => "First Name",
+    'profile-last-name' => 'Last Name',
+    'profile-gender' => 'Gender',
+    'profile-dob' => 'DOB',
+    'profile-phone' => 'Phone',
+    'profile-update' => 'Update',
+    'profile-date-check' => 'Sorry ! Minors need parential guidance to use this website',
+
+
+    'orders-my-account' => 'My Account',
+    'orders-my-order'=>'My Order',
+    'orders-date' => 'Order Date',
+    'orders-description' => 'Description',
+    'orders-order-id'=>'Order ID',
+    'orders-status'=>'Status',
+    'orders-detail'=>'Detail',
+
+
+    'order-detail' =>"Order Detail",
+    'order-detail-heading'=>"Order Information",
+    'order-detail-my-account' => 'My Account',
+    'order-detail-order-id' => 'Order ID',
+    'order-detail-order-status' => 'Order Status',
+    'order-detail-order-date' => 'Order Date',
+    'order-detail-shipping-detail' => 'Shipping Details',
+    'order-detail-billing-detail' => 'Billing Details',
+    'order-detail-payment-shipping-method' => 'Payment',
+    'order-detail-item' => 'ITEM"S',
+    'order-detail-price' => 'PRICE',
+    'order-detail-qty' => "QTY",
+    'order-detail-subtotal' => 'SUBTOTAL',
+    'order-detail-comments' => 'Comments',
+
+
+    'thank-you-title'=>'Thank You',
+    'thank-you-message' => 'You have successfully place your order.
+    Go to the',
+    'thank-you-order-page' => 'Order page',
+
+
+    'shipping-address'=>'Shipping Address',
+    'shipping-address-my-account' =>'My Account',
+    'shipping-address-default' =>'DEFAULT',
+    'shipping-address-first-name' =>'First Name',
+    'shipping-address-last-name' =>'Last Name',
+    'shipping-address-country-state-city' =>'Country State City',
+    'shipping-address-action' =>'ACTION',
+    'shipping-address-add' =>'Manage Shipping Address',
+    'shipping-address-stret-address' =>'Street Address',
+    'shipping-address-country' =>'Select Country',
+    'shipping-address-state' =>'Select State',
+    'shipping-address-city' =>'City',
+    'shipping-address-postal-code' =>'Postal Code',
+    'shipping-address-add-address' =>'Submit',
+
+
+
+    'change-password'=>'Change Password',
+    'change-password-current-password'=>'Current Password',
+    'change-password-new-password'=>'New Password',
+    'change-password-confirm-password'=>'Confirm Password',
+    'change-password-access-text'=>'Access Your Account Through Your Social Networks',
+
+
+    'forget-password'=>'Forget Password',
+    'forget-password-email'=>'Email',
+    'forget-password-verify'=>"varify email",
+
+
+    'bread-crumb-home'=>"Home",
+    'bread-crumb-shop'=>'Produtos',
+    'bread-shopping-cart'=>'SHOPPING CART',
+    'bread-product-page'=>'Product Page',
+    'bread-blog'=>'Novidades',
+    'bread-checkout'=>'Checkout',
+    'bread-login'=>'login',
+    'bread-compare'=>'Compare',
+    'bread-profile'=>'Profile',
+    'bread-order'=>'Order',
+    'wishlist-add-success' => 'Product Added To Wishlist!',
+    'wishlist-remove' => 'Product Remove From Wishlist!',
+
+
+
+
+
+];
